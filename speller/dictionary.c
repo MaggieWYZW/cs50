ec3b8aa24b7f6e83ec1cc0e215e35312ac1b39b6
Yeziwei Wang <yeziwei.wang@gmail.com>
2018-10-07 20:26:23 +0000

    speller trie implementation

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -8,37 +8,45 @@
 
 #include "dictionary.h"
 
-// Node for linked list
+#define ALPHABET_SIZE 27
+
+// Node for trie
 typedef struct node
 {
-    bool is_node;
+    bool is_word;
     struct node *children[27];
 }
 node;
-// Define a hashtable: array of nodes
-node *root;
 
-int index(char c)
-{
-    if (c < 'z' && c > 'a')
-    {
-        return (int) c-'a';
-    }
-    else if (c == '\'')
-    {
-        return 26;
-    }
-    else
-    {
-        return (int) c-'A';
-    }
-}
+// Create a root node
+static node *root;
+int INDEX(char c);
+node *createNode();
+bool isLeaf(node *n);
+bool unloadNode(node *n);
+
+unsigned int word_count = 0;
+
 
 // Returns true if word is in dictionary else false
 bool check(const char *word)
 {
-    return false;
+    node *curser = root;
+    for (int i=0, wordl=strlen(word); i<wordl; i++)
+    {
+        int c_index = INDEX(word[i]);
+        if (curser->children[c_index] != NULL)
+        {
+            curser = curser->children[c_index];
+        }
+        else
+        {
+            return false;
+        }
+    }
+    return curser->is_word;
 }
+
 
 // Loads dictionary into memory, returning true if successful else false
 bool load(const char *dictionary)
@@ -51,61 +59,107 @@ bool load(const char *dictionary)
         return false;
     }
 
-    //Load into a hash table
-    // Scan dictionary word by word
+    //Load into a trie
+    // Scan dictionary word by word, add to trie character by character
     char word[LENGTH+1];
+    root = createNode();
     while (fscanf(file, "%s", word) != EOF)
     {
         node *curser = root;
-        // Create a new node
-        for(int i = 0; i < strlen(word); i++)
+        for(int i = 0, wordl=strlen(word); i < wordl; i++)
         {
-            if (curser == NULL)
+            int c_index = INDEX(word[i]);
+            if (curser->children[c_index] == NULL)  // character doesn't exist in children
             {
-                node *new_node = malloc(sizeof(node));
-                curser->children[index(word[i])] = new_node;
+                node *new_node = createNode();
+                if (new_node == NULL)  // error allocate new node
+                {
+                    unload();
+                    return false;
+                }
+                // success allocate a new node
+                curser->children[c_index] = new_node;
             }
-            else if (curser->children[index(word[i])] == NULL && (i < strlen(word)-1))
-            {
-                node *new_node = malloc(sizeof(node));
-                curser->children[index(word[i])] = new_node;
-                curser = curser->children[index(word[i])];
-            }
-            else if ((i==strlen(word)-1) && curser->children[index(word[i])] == NULL)
-            {
-                node *new_node = malloc(sizeof(node));
-                curser->children[index(word[i])] = new_node;
-                curser->children[index(word[i])]->is_word = true;
-            }
-            else if (i==strlen(word)-1)
-            {
-                curser->is_word = true;
-            }
-            else
-            {
-                curser = curser->children[indes(word[i])];
-            }
-        }
+            curser = curser->children[c_index];
 
+        }
+        curser->is_word = true;
+        word_count++;
     }
     fclose(file);
     return true;
 }
 
+
+
+
 // Returns number of words in dictionary if loaded else 0 if not yet loaded
 unsigned int size(void)
 {
-    // If the dictionary is not loaded, return 0
-    int counter = 0;
-    return counter;
+    return word_count;
 }
+
 
 // Unloads dictionary from memory, returning true if successful else false
 bool unload(void)
 {
-
-    return true;
+    if (root == NULL)
+    {
+        return true;
+    }
+    return unloadNode(root);
 }
 
 
 
+// Create a new empty node
+node *createNode()
+{
+    node *tmp = malloc(sizeof(node));
+    if (tmp == NULL)
+    {
+        return NULL;
+    }
+
+    // set values
+    tmp->is_word = false;
+    for (int i = 0; i < ALPHABET_SIZE; i++)
+    {
+        tmp->children[i] = NULL;
+    }
+    return tmp;
+}
+
+
+
+// Compute the index of the character
+int INDEX(const char c)
+{
+    const char ch = tolower(c);
+    if (ch == '\'')
+    {
+        return ALPHABET_SIZE-1;
+    }
+    else
+    {
+        return (int)ch - (int)'a';
+    }
+
+}
+
+
+
+
+bool unloadNode(node *curser)
+{
+    // for each node, check all children to be null
+    for (int j = 0; j < ALPHABET_SIZE; j++)
+    {
+        if (curser->children[j] != NULL)
+        {
+            unloadNode(curser->children[j]);
+        }
+    }
+    free(curser);
+    return true;
+}
